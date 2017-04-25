24229c573c52f134fab3e3c0877114b3bdf5b5cf
Pavel Novotny <pavel.novotny.cz@gmail.com>
2017-04-25 17:48:11 +0200

    doplneni kopirovani main js z modulu

--- updateFromGit.sh
+++ updateFromGit.sh
@@ -1,5 +1,6 @@
 npm update
 cp node_modules/log-synchronize/config-LXCIPPPT401-CIP.json ./
 cp node_modules/log-synchronize/config-LXCIPPPT401-NOE.json ./
+cp node_modules/log-synchronize/*.js ./
 cp node_modules/log-synchronize/*.sh ./
 chmod u+x ./*.sh
